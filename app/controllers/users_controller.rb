382093ff2461032089b50a90a131b6c85f6c7c3b
めっちゃくれは <metchakureha@kurehapro3.local>
2025-01-23 00:29:46 +0900

    cancelを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   def show
     session[:previous_url] = mypage_path
     @posts = current_user.posts.order(created_at: :desc).page(params[:page])
+    @is_likes_view = false
   end
 
   def likes
     session[:previous_url] = mypage_likes_path
     @liked_posts = Post.joins(:likes).where(likes: { user_id: current_user.id }).order("likes.created_at DESC").page(params[:page])
+    @is_likes_view = true
     render :show
   end
 end
