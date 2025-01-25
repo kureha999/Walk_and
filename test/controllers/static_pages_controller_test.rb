32b7fdd033d297c5adb0751f67fb2ade3d686a78
めっちゃくれは <metchakureha@kurehapro3.local>
2025-01-25 13:13:44 +0900

    RSpecをinstall

--- test/controllers/static_pages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get privacy" do
-  #   get static_pages_privacy_url
-  #   assert_response :success
-  # end
-end
