3901e386fd9904c3bb17625d482c12b775f1914c
めっちゃくれは <metchakureha@kurehapro3.local>
2025-02-25 23:18:44 +0900

    carrierwavw移行

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,14 @@ require "carrierwave/storage/file"
 require "carrierwave/storage/fog"
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = "fog/aws"
-    config.fog_directory  = "walk-and"
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-      region: "ap-northeast-1"
-    }
-  else
-    config.storage :file
-    config.enable_processing = true if Rails.env.development?
-    config.enable_processing = false if Rails.env.test?
-  end
+  config.fog_credentials = {
+    provider:              "AWS",
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    use_iam_profile:       true,
+    region:                "ap-northeast-1"
+  }
+  config.fog_directory  = "walk-and"
+  config.fog_public     = false
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
 end
